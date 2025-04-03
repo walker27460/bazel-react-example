112678b766d2bce77d6a3bfd310cd2e7dafca4c0
Yui Tanabe <walker27460@users.noreply.github.com>
2025-04-04 00:36:37 +0900

    chore: explicitly configure builtin

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -1,5 +1,47 @@
 import { defineConfig } from "@rsbuild/core"
 import { pluginReact } from "@rsbuild/plugin-react"
 export default defineConfig({
-    plugins: [pluginReact()]
+  plugins: [pluginReact()],
+  tools: {
+    rspack: {
+      module: {
+        rules: [
+          {
+            test: /\.ts?x$/,
+            exclude: [/node_modules/],
+            loader: "builtin:swc-loader",
+            options: {
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  jsx: true
+                },
+                transform: {
+                  react: {
+                    pragma: "React.createElement",
+                    pragmaFrag: "React.Fragment",
+                    throwIfNamespace: true,
+                    development: false,
+                    useBuiltins: false
+                  }
+                }
+              }
+            },
+            type: "javascript/auto"
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: "builtin:lightningcss-loader",
+                options: {
+                  targets: "defaults"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
 })
